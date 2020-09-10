8bc1c6d1e2f94402ccfa670ad576bd17ddcaba3b
389093982 <389093982@qq.com>
2020-09-10 09:37:17 +0800

    字符串模块示例代码优化

--- src/com/linkknown/string/ConcatStringTest.java
+++ src/com/linkknown/string/ConcatStringTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 /**
  * 字符串连接测试
  * @author Administrator
+ * 1.无论如何直接用“+”号连接字符串都是最慢的
+ * 2.在拼接少数字符串（不超过4个）的时候，concat效率是最高的
+ * 3.多个字符串拼接的时候，StringBuilder/StringBuffer的效率是碾压的
+ * 4.在不需要考虑线程安全问题的时候，使用StringBuilder的效率比StringBuffer更高
  *
  */
 public class ConcatStringTest {
@@ -53,4 +57,54 @@ public class ConcatStringTest {
 		System.out.println(sb.toString());
 	}
 	
+	
+	/**
+	 * 多种连接方式性能比较
+	 * 1.无论如何直接用“+”号连接字符串都是最慢的 
+	 * 2.在拼接少数字符串（不超过4个）的时候，concat效率是最高的
+	 * 3.多个字符串拼接的时候，StringBuilder/StringBuffer的效率是碾压的
+	 * 4.在不需要考虑线程安全问题的时候，使用StringBuilder的效率比StringBuffer更高
+	 */
+	@Test
+	public void testStringConcat () {
+		
+		// +
+		String initialStr = "";
+		long start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++) {
+			initialStr = initialStr + "0";
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(String.format("+ total cost time %d (ms)", end - start));
+		
+		
+		// concat
+		initialStr = "";
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++) {
+			initialStr = initialStr.concat("0");
+		}
+		end = System.currentTimeMillis();
+		System.out.println(String.format("concat total cost time %d (ms)", end - start));
+		
+		
+		// StringBuilder
+		StringBuilder sb = new StringBuilder();
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++) {
+			sb = sb.append("0");
+		}
+		end = System.currentTimeMillis();
+		System.out.println(String.format("StringBuilder total cost time %d (ms)", end - start));
+	
+		
+		// StringBuffer
+		StringBuffer sbf = new StringBuffer();
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++) {
+			sbf = sbf.append("0");
+		}
+		end = System.currentTimeMillis();
+		System.out.println(String.format("StringBuffer total cost time %d (ms)", end - start));
+	}
 }
